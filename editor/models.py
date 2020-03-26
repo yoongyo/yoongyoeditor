74ba183f274d20e8680756d6c54a3f682491d5a2
yoongyo <jyg0172@naver.com>
2020-03-26 23:13:58 +0900

    AD

--- editor/models.py
+++ editor/models.py
@@ -3,4 +3,10 @@ from ckeditor_uploader.fields import RichTextUploadingField
 
 
 class Editor(models.Model):
-    content = RichTextUploadingField()
+    content = RichTextUploadingField(blank=True,
+                                     null=True,
+                                     external_plugin_resources=[(
+                                         'youtube',
+                                         '/static/base/vendor/ckeditor_plugins/youtube/youtube/',
+                                         'plugin.js',
+                                     )])
