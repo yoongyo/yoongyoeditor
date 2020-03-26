e4de45f466a4e9f6a44481e3bae469011856fe82
yoongyo <jyg0172@naver.com>
2020-03-26 22:56:27 +0900

    ad

--- editor/admin.py
+++ editor/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from .models import Editor
+from django import forms
+from ckeditor.widgets import CKEditorWidget
 
 
 class EditorAdmin(admin.ModelAdmin):
+    content = forms.CharField(widget=CKEditorWidget)
+
+    class Meta:
+        model = Editor
+
     list_display = ['content']
 
 
